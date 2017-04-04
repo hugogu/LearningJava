ca345dd5e281cf882e43f9d89da87bd54df06bf2
Hugo Gu <hugogu@outlook.com>
2017-04-04 12:05:49 +0800

    Customize the creation of CacheManager by loading ehcache.xml

--- LearningSpring/src/hugo/learning/java/spring/Application.java
+++ LearningSpring/src/hugo/learning/java/spring/Application.java
@@ -1,9 +1,12 @@
 package hugo.learning.java.spring;
 
 import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
+import javax.cache.spi.CachingProvider;
 
 import org.ehcache.config.CacheRuntimeConfiguration;
 import org.ehcache.config.ResourcePools;
@@ -17,6 +20,8 @@ import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import java.net.URISyntaxException;
+
 @SpringBootApplication(scanBasePackages = {
         "hugo.learning.java.*"
 })
@@ -27,6 +32,13 @@ public class Application {
     }
 
     @Bean
+    public CacheManager getCachemanager() throws URISyntaxException {
+        final CachingProvider cachingProvider = Caching.getCachingProvider();
+
+        return cachingProvider.getCacheManager(getClass().getResource("/ehcache.xml").toURI(), getClass().getClassLoader());
+    }
+
+    @Bean
     public JCacheManagerCustomizer getCacheManagerCustomizer() {
         // Spring only support Object->Object cache.
         //     at org.springframework.cache.jcache.JCacheCacheManager.loadCaches(JCacheCacheManager.java:105)
@@ -34,21 +46,29 @@ public class Application {
         // ehcache can support specific type caching.
         //    http://www.ehcache.org/documentation/3.1/107.html
         return cacheManager -> {
-            final MutableConfiguration<Object, Object> configuration =
-                    new MutableConfiguration<>()
-                            .setTypes(Object.class, Object.class)
-                            .setStoreByValue(false)
-                            .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_MINUTE));
-            final Cache cache = cacheManager.createCache("books", configuration);
-
-            // Set entry limitation through ehcache APIs. JSR-107 doesn't seem to support it.
-            final Eh107Configuration<Object, Object> eh107Configuration = (Eh107Configuration<Object, Object>) cache.getConfiguration(Eh107Configuration.class);
-            final CacheRuntimeConfiguration<Object, Object> runtimeConfiguration = eh107Configuration.unwrap(CacheRuntimeConfiguration.class);
-            final ResourcePools pools = ResourcePoolsBuilder
-                    .newResourcePoolsBuilder()
-                    .heap(20L, EntryUnit.ENTRIES)
-                    .build();
-            runtimeConfiguration.updateResourcePools(pools);
+            final Cache cache = createCacheViaJCacheAPI(cacheManager, Object.class, Object.class);
+            configEhCacheViaCode(cache);
         };
+    }
+
+    private <K, V> Cache<K, V> createCacheViaJCacheAPI(final CacheManager manager, Class<K> keyClass, Class<V> valueClass) {
+        final MutableConfiguration<K, V> configuration =
+                new MutableConfiguration<K, V>()
+                        .setTypes(keyClass, valueClass)
+                        .setStoreByValue(false)
+                        .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_MINUTE));
+
+        return manager.createCache("books", configuration);
+    }
+
+    private <K, V> void configEhCacheViaCode(final Cache<K, V> cache) {
+        // Set entry limitation through ehcache APIs. JSR-107 doesn't seem to support it.
+        final Eh107Configuration<K, V> eh107Configuration = (Eh107Configuration<K, V>) cache.getConfiguration(Eh107Configuration.class);
+        final CacheRuntimeConfiguration<K, V> runtimeConfiguration = eh107Configuration.unwrap(CacheRuntimeConfiguration.class);
+        final ResourcePools pools = ResourcePoolsBuilder
+                .newResourcePoolsBuilder()
+                .heap(20L, EntryUnit.ENTRIES)
+                .build();
+        runtimeConfiguration.updateResourcePools(pools);
     }
 }
