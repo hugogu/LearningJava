849b470c27a8ce1aaab8c16d1e1ef07a1d93a842
Hugo Gu <hugogu@outlook.com>
2017-04-04 10:32:09 +0800

    Config pool size on heap via ehcache APIs.

--- LearningSpring/src/hugo/learning/java/spring/Application.java
+++ LearningSpring/src/hugo/learning/java/spring/Application.java
@@ -1,8 +1,15 @@
 package hugo.learning.java.spring;
 
+import javax.cache.Cache;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
+
+import org.ehcache.config.CacheRuntimeConfiguration;
+import org.ehcache.config.ResourcePools;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.jsr107.Eh107Configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,7 +39,16 @@ public class Application {
                             .setTypes(Object.class, Object.class)
                             .setStoreByValue(false)
                             .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_MINUTE));
-            cacheManager.createCache("books", configuration);
+            final Cache cache = cacheManager.createCache("books", configuration);
+
+            // Set entry limitation through ehcache APIs. JSR-107 doesn't seem to support it.
+            final Eh107Configuration<Object, Object> eh107Configuration = (Eh107Configuration<Object, Object>) cache.getConfiguration(Eh107Configuration.class);
+            final CacheRuntimeConfiguration<Object, Object> runtimeConfiguration = eh107Configuration.unwrap(CacheRuntimeConfiguration.class);
+            final ResourcePools pools = ResourcePoolsBuilder
+                    .newResourcePoolsBuilder()
+                    .heap(20L, EntryUnit.ENTRIES)
+                    .build();
+            runtimeConfiguration.updateResourcePools(pools);
         };
     }
 }
